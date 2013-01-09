fe1525e1b2f5205f41ece113568ae04f001f4e54
Mustaavalkosta <niko.hyrynsalmi@gmail.com>
2013-01-09 20:54:47 +0000

    initial commit

---
+++ products/AndroidProducts.mk
@@ -0,0 +1,2 @@
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/pizza_ace.mk
---
+++ products/pizza_ace.mk
@@ -0,0 +1,16 @@
+PRODUCT_NAME := pizza_ace
+PRODUCT_BRAND := pizzabean
+
+# Common Files
+PRODUCT_COPY_FILES += \
+    vendor/pizza/prebuilt/common/app/ApexLauncher-1.4.0.apk:system/app/ApexLauncher.apk \
+    vendor/pizza/prebuilt/common/app/NovaLauncher-2.0.apk:system/app/NovaLauncher.apk \
+    vendor/pizza/prebuilt/common/app/Cerberus-2.3.apk:system/app/Cerberus.apk \
+    vendor/pizza/prebuilt/common/app/SuperSU-0.9.9.apk:system/app/Superuser.apk \
+    vendor/pizza/prebuilt/common/xbin/su-0.9.9:system/xbin/su \
+    vendor/pizza/prebuilt/common/etc/hosts:system/etc/hosts \
+    vendor/pizza/prebuilt/common/etc/init.d/98pizzabean:system/etc/init.d/98pizzabean
+
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.config.nocheckin=1 \
+	ro.config.htc.nocheckin=1
