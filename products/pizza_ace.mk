373fa0940792bb2c192fe1f67d108caf2d403cc8
Mustaavalkosta <niko.hyrynsalmi@gmail.com>
2013-01-19 22:02:33 +0000

    don't copy SuperSU
    Change-Id: I87425f2695c0b325b75e18d847297a2820bfb666

--- products/pizza_ace.mk
+++ products/pizza_ace.mk
@@ -4,6 +4,4 @@ PRODUCT_BRAND := pizzabean
 # Common Files
 PRODUCT_COPY_FILES += \
     vendor/pizza/prebuilt/common/app/Cerberus-2.3.apk:system/app/Cerberus.apk \
-    vendor/pizza/prebuilt/common/app/SuperSU-0.9.9.apk:system/app/Superuser.apk \
-    vendor/pizza/prebuilt/common/xbin/su-0.9.9:system/xbin/su \
     vendor/pizza/prebuilt/common/etc/hosts:system/etc/hosts
