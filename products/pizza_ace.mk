58694a3bcf06def7ed374d2d1382d27dbff2b964
Mustaavalkosta <niko.hyrynsalmi@gmail.com>
2013-01-27 13:46:15 +0000

    add fstrim (@"a user")
    Change-Id: I1100a2e18175140de95b1d127edc00b0a78542e4

--- products/pizza_ace.mk
+++ products/pizza_ace.mk
@@ -4,7 +4,8 @@ PRODUCT_BRAND := pizzabean
 # Common Files
 PRODUCT_COPY_FILES += \
     vendor/pizza/prebuilt/common/app/Cerberus-2.3.apk:system/app/Cerberus.apk \
-    vendor/pizza/prebuilt/common/etc/hosts:system/etc/hosts
+    vendor/pizza/prebuilt/common/etc/hosts:system/etc/hosts \
+    vendor/pizza/prebuilt/common/bin/fstrim:system/bin/fstrim
 
 PRODUCT_PACKAGES += \
 	ZeroXBenchmark
