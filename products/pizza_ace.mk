ca1d4c2a6c4c6728233c226f9335f82e9084f792
Mustaavalkosta <niko.hyrynsalmi@gmail.com>
2013-01-31 14:02:20 +0000

    add crontab and init.d script to run periodical fstrim and sqlite3 Big chunk of init.d script is based on a user's work for CM 10.1 tweaks
    Change-Id: I51d9067c275af315baa785014a7fa09c2f0ca267

--- products/pizza_ace.mk
+++ products/pizza_ace.mk
@@ -5,7 +5,9 @@ PRODUCT_BRAND := pizzabean
 PRODUCT_COPY_FILES += \
     vendor/pizza/prebuilt/common/app/Cerberus-2.3.apk:system/app/Cerberus.apk \
     vendor/pizza/prebuilt/common/etc/hosts:system/etc/hosts \
-    vendor/pizza/prebuilt/common/bin/fstrim:system/bin/fstrim
+    vendor/pizza/prebuilt/common/bin/fstrim:system/bin/fstrim \
+    vendor/pizza/prebuilt/common/etc/init.d/99pizzabean:system/etc/init.d/99pizzabean \
+    vendor/pizza/prebuilt/common/etc/cron.d/crontabs/root:system/etc/cron.d/crontabs/root
 
 PRODUCT_PACKAGES += \
 	ZeroXBenchmark
