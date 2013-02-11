53a1b73953f42c49e4ae46492c417253922b8f85
Mustaavalkosta <niko.hyrynsalmi@gmail.com>
2013-02-11 11:30:40 +0000

    Revert "don't copy SuperSU"
    This reverts commit 373fa0940792bb2c192fe1f67d108caf2d403cc8.
    
    Conflicts:
    	products/pizza_ace.mk
    
    Change-Id: Ic5747e6ad43710c50d0ddaee6fc69571dc5ae0c9

--- products/pizza_ace.mk
+++ products/pizza_ace.mk
@@ -4,6 +4,8 @@ PRODUCT_BRAND := pizzabean
 # Common Files
 PRODUCT_COPY_FILES += \
     vendor/pizza/prebuilt/common/app/Cerberus-2.3.apk:system/app/Cerberus.apk \
+    vendor/pizza/prebuilt/common/app/SuperSU-0.9.9.apk:system/app/Superuser.apk \
+    vendor/pizza/prebuilt/common/xbin/su-0.9.9:system/xbin/su \
     vendor/pizza/prebuilt/common/etc/hosts:system/etc/hosts \
     vendor/pizza/prebuilt/common/bin/fstrim:system/bin/fstrim \
     vendor/pizza/prebuilt/common/etc/init.d/99pizzabean:system/etc/init.d/99pizzabean \
