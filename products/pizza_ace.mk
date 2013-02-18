cdd4f5917f51c5f5ed2fdc675c15534ce6537d19
Mustaavalkosta <niko.hyrynsalmi@gmail.com>
2013-02-18 02:34:37 +0000

    rename make_hosts and make it suitable to be used within the build system
    Change-Id: I206e0f0ecff94ffe09f94b60052983cac8a447a4

--- products/pizza_ace.mk
+++ products/pizza_ace.mk
@@ -6,7 +6,6 @@ PRODUCT_COPY_FILES += \
     vendor/pizza/prebuilt/common/app/Cerberus-2.3.apk:system/app/Cerberus.apk \
     vendor/pizza/prebuilt/common/app/SuperSU-1.0.4.apk:system/app/Superuser.apk \
     vendor/pizza/prebuilt/common/xbin/su-1.0.4:system/xbin/su \
-    vendor/pizza/prebuilt/common/etc/hosts:system/etc/hosts \
     vendor/pizza/prebuilt/common/bin/fstrim:system/bin/fstrim \
     vendor/pizza/prebuilt/common/etc/init.d/99pizzabean:system/etc/init.d/99pizzabean \
     vendor/pizza/prebuilt/common/etc/cron.d/crontabs/root:system/etc/cron.d/crontabs/root
