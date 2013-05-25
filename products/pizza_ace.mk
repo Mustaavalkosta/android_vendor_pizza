e581d53e3c0f0ed8ba5364ebbde5a70dcbd4f0fe
Mustaavalkosta <niko.hyrynsalmi@gmail.com>
2013-05-25 20:44:28 +0000

    Revert "build 0xbench"
    This reverts commit 430d037659dc0debcdbedeb14cdaae3a761e3165.
    
    Conflicts:
    	products/pizza_ace.mk
    
    Change-Id: I566748bbc26c56720c070c4ee45427cfb8a98ef3

--- products/pizza_ace.mk
+++ products/pizza_ace.mk
@@ -6,6 +6,3 @@ PRODUCT_COPY_FILES += \
     vendor/pizza/prebuilt/common/app/Cerberus-2.3.apk:system/app/Cerberus.apk \
     vendor/pizza/prebuilt/common/etc/init.d/99pizzabean:system/etc/init.d/99pizzabean \
     vendor/pizza/prebuilt/common/etc/cron.d/crontabs/root:system/etc/cron.d/crontabs/root
-
-PRODUCT_PACKAGES += \
-	ZeroXBenchmark
