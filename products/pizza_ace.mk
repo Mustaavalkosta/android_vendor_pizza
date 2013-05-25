dee2e977a5952a3658ca8e2d88badf636a84fbd9
Mustaavalkosta <niko.hyrynsalmi@gmail.com>
2013-05-25 20:45:35 +0000

    include Play Store version of 0xbench
    Change-Id: I82736e0374d63bd479890fd56756294422379f6e

--- products/pizza_ace.mk
+++ products/pizza_ace.mk
@@ -4,5 +4,6 @@ PRODUCT_BRAND := pizzabean
 # Common Files
 PRODUCT_COPY_FILES += \
     vendor/pizza/prebuilt/common/app/Cerberus-2.3.apk:system/app/Cerberus.apk \
+    vendor/pizza/prebuilt/common/app/ZeroXBenchmark.apk:system/app/ZeroXBenchmark.apk \
     vendor/pizza/prebuilt/common/etc/init.d/99pizzabean:system/etc/init.d/99pizzabean \
     vendor/pizza/prebuilt/common/etc/cron.d/crontabs/root:system/etc/cron.d/crontabs/root
